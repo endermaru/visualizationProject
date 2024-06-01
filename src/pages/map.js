dd825db4a04aef732b29576369ba7b68fb88f28e
endermaru <endermaru@snu.ac.kr>
2024-06-01 22:07:21 +0900

    서울 전경 트랜지션 추가

--- src/pages/map.js
+++ src/pages/map.js
@@ -3,30 +3,111 @@ import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
+export const coord = {
+  //seoul
+  init:{
+    lat:37.56786346227889,
+    lng:126.9756051435611,
+    bearing: 0,
+    pitch: 0,
+    zoom: 10.717126200821815
+  },
+  center:{
+    lat:37.55133914078637,
+    lng:126.9757847023518,
+    bearing: 0,
+    pitch: 53.54040022453836,
+    zoom: 15.527706568294564
+  },
+}
+
+//for markers
+const geojson = {
+  'type': 'FeatureCollection',
+  'features': [
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [126.978736, 37.586085]
+      },
+      'properties': {
+        'title': 'donui',
+        'marker':'marker1'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [127.026062,37.573729]
+      },
+      'properties': {
+        'title': 'changsin',
+        'marker':'marker2'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [126.974137,37.545535]
+      },
+      'properties': {
+        'title': 'dongja',
+        'marker':'marker3'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [126.907724,37.513448]
+      },
+      'properties': {
+        'title': 'yeongdeungpo',
+        'marker':'marker4'
+      }
+    }
+  ]
+};
+
 const MapB =(props) => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [visible, setVisible] = useState(true);
-
-  const lat_init = 37.55220205704455;
-  const lng_init = 126.974485672578;
-  const bear_init = -92.40525969468547;
-  const pitch_init = 61.5;
-  const zoom_init = 15.605997843300576;
-
+  const mapContainer = useRef(null); //맵 컨테이너 객체
+  const map = useRef(null); //맵 객체
+  const [visible, setVisible] = useState(true); //기타 건물 표시 여부
+  const markers = useRef([]); //마커 관리
   
+  //useEffect for mapbox
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+
+    //init
+    if (map.current) return;
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/endermaru/clw7k3lp201gl01ob8hri10ur',
-      center: [lng_init,lat_init],
-      zoom: zoom_init,
-      pitch: pitch_init,
-      bearing: bear_init,
+      center: [coord.init.lng,coord.init.lat],
+      zoom: coord.init.zoom,
+      pitch: coord.init.pitch,
+      bearing: coord.init.bearing,
       attributionControl: false
     });
+    //load - 레이어 표시 및 쪽방촌 마커 추가
+    map.current.on('style.load', () => {
+      map.current.setLayoutProperty('seoul1', 'visibility', 'visible');
+      map.current.setLayoutProperty('seoul2', 'visibility', 'visible');
+      
+      for (const feature of geojson.features) {
+        const el = document.createElement('div');
+        el.className = feature.properties.marker;
+        const marker = new mapboxgl.Marker(el)
+          .setLngLat(feature.geometry.coordinates)
+          .addTo(map.current);
+        markers.current.push(marker);
+      }
+    });
 
+    //팝업
     //Valid others
     map.current.on('click', 'valid_others', (e) => {
       let address = e.features[0].properties.address.slice(5,);
@@ -51,7 +132,6 @@ const MapB =(props) => {
     map.current.on('mouseleave', 'valid_others', () => {
       map.current.getCanvas().style.cursor = '';
     });
-
     //Valid target
     map.current.on('click', 'valid_target', (e) => {
       let address = e.features[0].properties.address.slice(5,);
@@ -70,9 +150,6 @@ const MapB =(props) => {
           </div>`)
         .addTo(map.current);
     });
-    
-
-    
     map.current.on('mouseenter', 'valid_target', () => {
       map.current.getCanvas().style.cursor = 'pointer';
     });
@@ -80,6 +157,7 @@ const MapB =(props) => {
       map.current.getCanvas().style.cursor = '';
     });
 
+    //실시간 좌표 출력
     map.current.on('move',(e)=>{
       const center = map.current.getCenter(); 
       const bearing = map.current.getBearing();
@@ -88,27 +166,28 @@ const MapB =(props) => {
 
       console.log('Current center:', center);
       console.log('Current bearing:', bearing);
-      console.log('Current zooom', zoom);
       console.log('Current Pitch', pitch);
-    })
-    // Reset button functionality
+      console.log('Current zooom', zoom);
+    });
+    
+    //처음 위치로 돌아가기
     document.getElementById('reset').addEventListener('click', () => {
       map.current.flyTo({
-        center: [lng_init,lat_init],
-        zoom: zoom_init,
-        pitch: pitch_init,
-        bearing: bear_init,
+        center: [coord.init.lng,coord.init.lat],
+        zoom: coord.init.zoom,
+        pitch: coord.init.pitch,
+        bearing: coord.init.bearing,
         essential: true
       });
     });
-
-    map.current.addControl(new mapboxgl.FullscreenControl());
-    map.current.addControl(new mapboxgl.NavigationControl());
-    
+    map.current.addControl(new mapboxgl.NavigationControl())
   });
 
+  //action - index와 통신
   useEffect(() => {
-    if (map.current) {
+    
+    //이동
+    setTimeout(()=>{if (map.current) {
       map.current.flyTo({
         center: [props.lng, props.lat],
         zoom: props.zoom,
@@ -117,8 +196,28 @@ const MapB =(props) => {
         essential: true,
       });
     }
+    },300);
+
+    //레이어 비활성화 및 마커 제거
+    if (props.action=='center'){
+      markers.current.forEach(marker => {
+        const markerElement = marker.getElement();
+        markerElement.classList.add('fade-out');
+        markerElement.addEventListener('animationend', () => {
+          marker.remove();
+        });
+      });
+      markers.current = [];
+
+      const visi = map.current.getLayoutProperty('seoul1', 'visibility');
+      if (visi=='visible'){
+        map.current.setLayoutProperty('seoul1', 'visibility', 'none');
+        map.current.setLayoutProperty('seoul2', 'visibility', 'none');
+      }
+    }
   }, [props.action]); 
 
+  //기타 건물 활성화, 비활성화
   const visibleToggle = (e)=>{
     const popup = document.getElementsByClassName('mapboxgl-popup');
     if (popup.length) {
@@ -126,28 +225,32 @@ const MapB =(props) => {
     }
     // Toggle layer visibility by changing the layout object's visibility property.
     if (visible) {
+      map.current.setLayoutProperty('valid_others_inactive', 'visibility', 'visible');
       map.current.setLayoutProperty('valid_others', 'visibility', 'none');
+      map.current.setLayoutProperty('valid_target_icon', 'visibility', 'none');
     } else {
       map.current.setLayoutProperty('valid_others', 'visibility', 'visible');
+      map.current.setLayoutProperty('valid_others_inactive', 'visibility', 'none');
+      map.current.setLayoutProperty('valid_target_icon', 'visibility', 'visible');
     }
     setVisible(!visible);
   }
 
   return (
     <>
-    <div className="absolute flex flex-col z-10 top-36 right-0 px-1 rounded-md">
-    <button className={`rounded-md p-1 m-1 aspect-square border border-1 border-black bg-white font-bold text-stone-700 hover:text-blue-600`
-    } onClick={(e)=>visibleToggle(e)}>
-        <p className="text-2xl">{!visible? "🗺️":"🔍"}</p>
-        <p className="text-xs">{!visible? "다른건물":"쪽방촌만"}</p>
-        <p className="text-xs">{!visible? "둘러보기":"살펴보기"}</p>
-    </button>
-    <button className="parent rounded-md p-1 m-1 aspect-square border border-1 border-black bg-white hover:bg-white font-bold text-stone-700 hover:text-blue-600" id="reset">
-        <p className="text-2xl rotate-on-hover">↻</p>
-        <p className="text-xs">처음으로</p>
-    </button>
-    </div>
-    <div className="z-0" ref={mapContainer} style={{ width: '100vw', height: '100vh' }} />
+      <div className="absolute flex flex-col z-10 top-36 right-0 px-1 rounded-md">
+      <button className={`rounded-md p-1 m-1 aspect-square border border-1 border-black bg-white font-bold text-stone-700 hover:text-blue-600`
+      } onClick={(e)=>visibleToggle(e)}>
+          <p className="text-2xl">{!visible? "🗺️":"🔍"}</p>
+          <p className="text-xs">{!visible? "다른건물":"쪽방촌만"}</p>
+          <p className="text-xs">{!visible? "둘러보기":"살펴보기"}</p>
+      </button>
+      <button className="parent rounded-md p-1 m-1 aspect-square border border-1 border-black bg-white hover:bg-white font-bold text-stone-700 hover:text-blue-600" id="reset">
+          <p className="text-2xl rotate-on-hover">↻</p>
+          <p className="text-xs">처음으로</p>
+      </button>
+      </div>
+      <div className="z-0" ref={mapContainer} style={{ width: '100vw', height: '100vh' }} />
     </>
   );
 }
